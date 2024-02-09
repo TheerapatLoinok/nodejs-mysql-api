0410f7abaea717f8d482794f91138de8e0b59671
theerapatloinok <theerapat.l@kkumail.com>
2024-02-09 15:36:34 +0700

    [main] upload image : create upload image api

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const app = express();
 const bodyParser = require("body-parser");
 const postRoute = require("./routes/posts");
 const userRoute = require("./routes/users");
+const imageRoute = require("./routes/images");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/uploads", express.static("uploads"));
 
 app.get("/health-check", (req, res) => {
   res.status(200).json({ message: "Server start with port 3000" });
 });
 app.use("/posts", postRoute);
 app.use("/user", userRoute);
+app.use("/images", imageRoute);
 
 module.exports = app;
